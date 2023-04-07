e4d667eb62062226d956280d0321bf5f2497a95e
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-04-07 15:45:46 -0500

    Initial commit

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const Form = () => {
+    const [zipcode, setZipCode] = useState('')
+    const navigate = useNavigate();
+    
+    
+       const handleSubmit = async(e) =>{
+        e.preventDefault();
+        setZipCode('')  
+try {
+  const weatherData = await axios.post('/', {zipcode})
+  navigate('/weather', {state: {data: weatherData.data}});
+} catch (error) {
+  if(error){
+    alert('Zipcode is incorrect!')
+  }
+}
+     
+        
+
+
+
+        
+       
+      
+       
+
+  }
+
+
+  return (
+    <div className='w-full h-[100vh]'>
+    <h1 className='text-center p-5 text-[8vmin] font-bold'>ENTER ZIPCODE</h1>
+
+    <div className='w-full h-[55%] flex justify-center items-center'>
+
+        <form onSubmit={handleSubmit}>
+          
+        <input className='border border-black rounded-md' value={zipcode} type = 'text' onChange = {e=>setZipCode(e.target.value)} placeholder={'Zipcode...'} required/>
+        <button className='rounded-md bg-green-500'>Search</button>
+   </form>
+    </div>
+    </div>
+  )
+}
+
+export default Form
+
+
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const hostname = 'localhost'
+const port = 5000;
+require('dotenv').config();
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen(port, hostname, ()=>{
+    console.log(`The server is running on ${hostname}${port}`)
+})
+
+app.post('/', async(req, res)=>{
+const {zipcode} = req.body
+try {
+    if (zipcode.length < 5 || zipcode.length > 5){
+        return res.status(400).json({message: 'Zipcode provided is incorrect!'})
+    }
+    else{
+        const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${process.env.api_key}&units=imperial`)
+        res.send(weather.data)
+       
+    }
+} catch (error) {
+    console.log(error)
+    res.status(400).json({message: "Zipcode is Incorrect"})
+}
+
+}
+)
