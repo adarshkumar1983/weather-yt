216be06ae748e471fe1e68f0d747f02372a36b46
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-04-27 16:29:53 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const hostname = 'localhost'
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
@@ -12,8 +11,8 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.listen(port, hostname, ()=>{
-    console.log(`The server is running on ${hostname}${port}`)
+app.listen(port, ()=>{
+    console.log(`The server is running on ${port}`)
 })
 
 app.post('/', async(req, res)=>{
